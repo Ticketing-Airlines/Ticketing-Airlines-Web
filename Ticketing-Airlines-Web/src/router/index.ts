7b75f609ef1c03d19b1562c8f74465961525533b
Nicole Keith Inot <inotnicole93@gmail.com>
2025-11-27 02:49:10 +0800

    feat: update AppFooter to use RouterLink for navigation and add smooth scroll behavior to router

--- Ticketing-Airlines-Web/src/router/index.ts
+++ Ticketing-Airlines-Web/src/router/index.ts
@@ -2,6 +2,14 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  scrollBehavior(to, from, savedPosition) {
+    // Always scroll to top when navigating to a new route
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0, behavior: 'smooth' }
+    }
+  },
   routes: [
     {
       path: '/',
